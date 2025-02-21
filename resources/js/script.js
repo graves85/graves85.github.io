90b3c7e567d2e9b0ae83f0ee081301bdb011fcc9
XY <yunyun28@naver.com>
2025-02-22 00:57:24 +0900

    fix: 목록 노출 오류 수정

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,16 +6,16 @@ fetch('/data/posts.json')
     posts.forEach(post => {
         // 각 포스트를 담을 div 요소 생성
         const postDiv = document.createElement('div');
-        postDiv.classList.add('post'); // 'post' 클래스 추가
+        postDiv.classList.add('post-item'); // 'post-item' 클래스 추가
 
         // 포스트 제목 추가
         const postTitle = document.createElement('h4');
         postTitle.textContent = post.title;
         postDiv.appendChild(postTitle);
 
-        // 포스트 내용 추가
+        // 포스트 날짜 추가
         const postDate = document.createElement('p');
-        postDate.date = post.date;
+        postDate.textContent = post.date;
         postDiv.appendChild(postDate);
 
         // main 태그 내에 새로 생성한 div 추가
